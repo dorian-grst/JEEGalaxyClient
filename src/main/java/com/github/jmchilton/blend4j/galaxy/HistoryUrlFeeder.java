d21e3ff373d58bb31ad38e45f5e2e577cf145fb4
Dorian Grasset <dorian.grasset@cirad.fr>
2024-03-26 10:46:58 +0100

    add isLocal test for the file url

--- src/main/java/com/github/jmchilton/blend4j/galaxy/HistoryUrlFeeder.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/HistoryUrlFeeder.java
@@ -2,11 +2,16 @@ package com.github.jmchilton.blend4j.galaxy;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.codehaus.jackson.annotate.JsonProperty;
 
@@ -14,83 +19,105 @@ import com.sun.jersey.api.client.ClientResponse;
 
 public class HistoryUrlFeeder extends ToolsClientImpl {
 
-	public HistoryUrlFeeder(GalaxyInstance galaxyInstance) {
-		super((GalaxyInstanceImpl) galaxyInstance);
-	}
+    public HistoryUrlFeeder(GalaxyInstance galaxyInstance) {
+        super((GalaxyInstanceImpl) galaxyInstance);
+    }
 
-	public ClientResponse historyUrlFeedRequest(final UrlFileUploadRequest request) throws IOException {
-		final Map<String, String> uploadParameters = new HashMap<String, String>();
-		String fileUrl = request.getFileUrl();
-		if (fileUrl.contains("http")) {
-			uploadParameters.put("files_0|url_paste", fileUrl);
-		} else {
-			String fileContent = FileUtils.readFileToString(new File(fileUrl));
-			uploadParameters.put("files_0|url_paste", fileContent);
-		}
-		uploadParameters.put("files_0|NAME", Paths.get(request.getFileUrl()).getFileName().toString());
-		uploadParameters.put("dbkey", request.getDbKey());
-		uploadParameters.put("file_type", request.getFileType());
-		uploadParameters.putAll(request.getExtraParameters());
-		final Map<String, Object> requestParameters = new HashMap<String, Object>();
-		requestParameters.put("tool_id", request.getToolId());
-		requestParameters.put("history_id", request.getHistoryId());
-		requestParameters.put("inputs", write(uploadParameters));
-		requestParameters.put("type", "upload_dataset");
-		return multipartPost(getWebResource(), requestParameters, prepareUploads(Arrays.asList()));
-	}
+    public ClientResponse historyUrlFeedRequest(final UrlFileUploadRequest request) throws IOException {
+        final Map<String, String> uploadParameters = new HashMap<String, String>();
+        String fileUrl = request.getFileUrl();
+        if (fileUrl.startsWith("http")) {
+            if (isLocalURL(fileUrl)) {
+                uploadParameters.put("files_0|url_paste", IOUtils.toString(new URL(fileUrl), "UTF-8"));
+            } else {
+                uploadParameters.put("files_0|url_paste", fileUrl);
+            }
+        } else {
+            String fileContent = FileUtils.readFileToString(new File(fileUrl));
+            uploadParameters.put("files_0|url_paste", fileContent);
+        }
+        uploadParameters.put("files_0|NAME", Paths.get(request.getFileUrl()).getFileName().toString());
+        uploadParameters.put("dbkey", request.getDbKey());
+        uploadParameters.put("file_type", request.getFileType());
+        uploadParameters.putAll(request.getExtraParameters());
+        final Map<String, Object> requestParameters = new HashMap<String, Object>();
+        requestParameters.put("tool_id", request.getToolId());
+        requestParameters.put("history_id", request.getHistoryId());
+        requestParameters.put("inputs", write(uploadParameters));
+        requestParameters.put("type", "upload_dataset");
+        return multipartPost(getWebResource(), requestParameters, prepareUploads(Arrays.asList()));
+    }
 
-	public static class UrlFileUploadRequest {
-		private final String historyId;
-		private final String fileUrl;
-		private String fileType = "auto";
-		private String dbKey = "?";
-		private String toolId = "upload1";
-		private Map<String, String> extraParameters = new HashMap<String, String>();
+    public static boolean isLocalURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            String host = url.getHost();
+            InetAddress localhost = InetAddress.getLocalHost();
+            InetAddress[] allLocal = InetAddress.getAllByName(localhost.getCanonicalHostName());
+            for (InetAddress local : allLocal) {
+                if (local.getHostAddress().equals(host) || host.equals("localhost")) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (MalformedURLException | UnknownHostException e) {
+            e.printStackTrace();
+            return false; // Consider it as non-local if URL is malformed or host address cannot be resolved
+        }
+    }
 
-		public Map<String, String> getExtraParameters() {
-			return extraParameters;
-		}
+    public static class UrlFileUploadRequest {
+        private final String historyId;
+        private final String fileUrl;
+        private String fileType = "auto";
+        private String dbKey = "?";
+        private String toolId = "upload1";
+        private Map<String, String> extraParameters = new HashMap<String, String>();
 
-		public void setExtraParameters(final Map<String, String> extraParameters) {
-			this.extraParameters = extraParameters;
-		}
+        public Map<String, String> getExtraParameters() {
+            return extraParameters;
+        }
 
-		public UrlFileUploadRequest(final String historyId, final String fileUrl) {
-			this.historyId = historyId;
-			this.fileUrl = fileUrl;
-		}
+        public void setExtraParameters(final Map<String, String> extraParameters) {
+            this.extraParameters = extraParameters;
+        }
 
-		public String getFileType() {
-			return fileType;
-		}
+        public UrlFileUploadRequest(final String historyId, final String fileUrl) {
+            this.historyId = historyId;
+            this.fileUrl = fileUrl;
+        }
 
-		public void setFileType(String fileType) {
-			this.fileType = fileType;
-		}
+        public String getFileType() {
+            return fileType;
+        }
 
-		public String getDbKey() {
-			return dbKey;
-		}
+        public void setFileType(String fileType) {
+            this.fileType = fileType;
+        }
 
-		public void setDbKey(String dbKey) {
-			this.dbKey = dbKey;
-		}
+        public String getDbKey() {
+            return dbKey;
+        }
 
-		public String getToolId() {
-			return toolId;
-		}
+        public void setDbKey(String dbKey) {
+            this.dbKey = dbKey;
+        }
 
-		public String getHistoryId() {
-			return historyId;
-		}
+        public String getToolId() {
+            return toolId;
+        }
 
-		@JsonProperty("files_0|url_paste")
-		public String getFileUrl() {
-			return fileUrl;
-		}
+        public String getHistoryId() {
+            return historyId;
+        }
 
-		public void setToolId(String toolId) {
-			this.toolId = toolId;
-		}
-	}
+        @JsonProperty("files_0|url_paste")
+        public String getFileUrl() {
+            return fileUrl;
+        }
+
+        public void setToolId(String toolId) {
+            this.toolId = toolId;
+        }
+    }
 }
